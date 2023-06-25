6753cde6f1452c5c55001d0e38ef069dc2368937
KmiloZ <3107320340407@ingenieria.usac.edu.gt>
2023-06-25 16:14:49 -0600

    Delete, Delete all, Rename y Modify
    Se agregan los comandos descritos en el titulo para su control en maquina virtual

--- Prueba.py
+++ Prueba.py
@@ -5,33 +5,76 @@ import regex as re
 
 dir_origen = "./Archivos"
 #------------------------------------------------------------FUNCIONES DE COMANDOS---------------------------------------------------------
-def fun_crear(nombre_archivo,contenido_archivo,ruta_archivo,tipo_accion):
-    tipo = tipo_accion.lower()
+def fun_crear(nombre_archivo,contenido_archivo,ruta_archivo,):
     #CODIGO EN CASO DE QUE SE EJECUTE EN SERVIDOR
-    if(tipo == "server"):
-        ruta_archivo_limpia = limpiar_ruta(ruta_archivo)
-        #Se crean las carpetas necesarias
-        crear_carpeta(ruta_archivo_limpia)
-        #Creacion de archivo
-        if os.path.exists(ruta_archivo_limpia+nombre_archivo):
-            nombre_archivo = nombre_archivo+"(1)"
-        f = open(ruta_archivo_limpia+nombre_archivo,"x")
-        f.write(contenido_archivo)
-        f.close()
-    #CODIGO EN CASO DE QUE SE EJECUTE EN BUCKET
+    ruta_archivo_limpia = limpiar_ruta(ruta_archivo)
+    #Se crean las carpetas necesarias
+    crear_carpeta(ruta_archivo_limpia)
+    #Creacion de archivo
+    if os.path.exists(ruta_archivo_limpia+nombre_archivo):
+        nombre_archivo = nombre_archivo+"(1)"
+    f = open(ruta_archivo_limpia+nombre_archivo,"x")
+    f.write(contenido_archivo)
+    f.close()
 
-def fun_eliminar(ruta_archivo,nombre_archivo,tipo_accion):
-    tipo = tipo_accion.lower()
+def fun_eliminar(ruta_archivo,nombre_archivo):
     #CODIGO EN CASO DE QUE SE EJECUTE EN SERVIDOR
-    if(tipo == "server"):
-        ruta_archivo_limpia = limpiar_ruta(ruta_archivo)
-        ruta_eliminar = ruta_archivo_limpia+nombre_archivo
-        if os.path.exists(ruta_eliminar):
-            if os.path.isfile(ruta_eliminar):
-                os.remove(ruta_eliminar)
-            else:
-                rmtree(ruta_eliminar)
-    #CODIGO EN CASO DE QUE SE EJECUTE EN BUCKET
+    ruta_archivo_limpia = limpiar_ruta(ruta_archivo)
+    ruta_eliminar = ruta_archivo_limpia+nombre_archivo
+    if os.path.exists(ruta_eliminar):
+        if os.path.isfile(ruta_eliminar):
+            os.remove(ruta_eliminar)
+        else:
+            rmtree(ruta_eliminar)
+
+def fun_renombrar(ruta,nuevo_nombre):
+    #Se limpia la ruta en caso alguna parte tenga doble comilla
+    nueva_ruta = limpiar_ruta(ruta)
+    #Se reestructura la ruta para el nuevo nombre
+    partes = nueva_ruta.split("/")
+    nueva_partes = [x for x in partes if x != '']
+    rta = ""
+    for x in nueva_partes:
+        if not re.search(".*\.txt",x):
+            rta=rta+x+"/"
+        else:
+            dir_carpeta = rta
+            rta+=nuevo_nombre
+            break
+    continuar = True
+    if os.path.exists(nueva_ruta):
+        #Se compara archivo existentes para verificar si no se repiten
+        for x in os.listdir(dir_carpeta):
+            if(x == nuevo_nombre):
+                continuar = False
+                break
+        if continuar:
+            os.rename(nueva_ruta,rta)
+        else:
+            print("Imposible Renombrar, Existe un Archivo con el mismo nombre")
+    else:
+        print("Imposible de Renombrar, El Directorio o Archivo No Existe")
+
+def fun_modificar(ruta,nuevo_contenido):
+    #Se limpia la ruta en caso alguna parte tenga doble comilla
+    nueva_ruta = limpiar_ruta(ruta)
+    if os.path.exists(nueva_ruta):
+        f = open(nueva_ruta,"w")
+        f.write(nuevo_contenido)
+        f.close()
+    else:
+        print("El Directorio o Archivo No Existe")
+
+def fun_eliminar_todo():
+    for filename in os.listdir(dir_origen+"/"):
+        ruta = os.path.join(dir_origen+"/",filename)
+        try:
+            if os.path.isfile(ruta) or os.path.islink(ruta):
+                os.unlink(ruta)
+            elif os.path.isdir(ruta):
+                rmtree(ruta)
+        except Exception as e:
+            print("Error "+e)
 #-----------------------------------------------------------FUNCIONES COMPLEMENTARIAS---------------------------------------------------------
 def limpiar_ruta(ruta_archivo):
     partes = ruta_archivo.split("/")
@@ -55,5 +98,8 @@ def crear_carpeta(ruta):
         if not os.path.exists(rta) and not re.search(".*\.txt",x):
             os.makedirs(rta)
 
-#fun_crear("archivo_n.txt","contenido_archivo","/carpeta1","server")
-fun_eliminar("/carpeta1","","server")
+#fun_crear("archivo_n.txt","contenido_archivo","/carpeta1")
+#fun_eliminar("/carpeta1","")
+#fun_renombrar("/carpeta1/archivo_n.txt","archivo_renombre.txt")
+#fun_modificar("/carpeta1/archivo_renombre.txt","Nuevo contenido")
+fun_eliminar_todo()
